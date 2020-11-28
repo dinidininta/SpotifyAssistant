8da36f2224b1b2c0040f25cdc39dd3bbeee335ae
Dininta Isnurthina <dini.dininta@gmail.com>
2020-11-28 17:59:30 +0700

    Add button to start PlaylistActivity. Change android:text to tools:text.

--- app/src/main/java/com/dinidininta/spotifyassistant/MainActivity.java
+++ app/src/main/java/com/dinidininta/spotifyassistant/MainActivity.java
@@ -2,8 +2,10 @@ package com.dinidininta.spotifyassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,11 +19,17 @@ public class MainActivity extends AppCompatActivity {
         TextView birthdate = findViewById(R.id.birthdate);
         TextView country = findViewById(R.id.country);
         TextView email = findViewById(R.id.email);
+        Button openPlaylist = findViewById(R.id.btn_open_playlist);
 
         SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
         displayName.setText(sharedPreferences.getString("displayName", ""));
         birthdate.setText(sharedPreferences.getString("birthdate", ""));
         country.setText(sharedPreferences.getString("country", ""));
         email.setText(sharedPreferences.getString("email", ""));
+
+        openPlaylist.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PlaylistsActivity.class);
+            startActivity(intent);
+        });
     }
 }
