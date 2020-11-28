94066b1f03f9a0bf1155153644bf1b2104833bd3
Dininta Isnurthina <dini.dininta@gmail.com>
2020-11-28 18:23:33 +0700

    Create and populate PlaylistsAdapter for recyclerView

--- app/src/main/java/com/dinidininta/spotifyassistant/PlaylistsActivity.java
+++ app/src/main/java/com/dinidininta/spotifyassistant/PlaylistsActivity.java
@@ -1,14 +1,35 @@
 package com.dinidininta.spotifyassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.dinidininta.spotifyassistant.POJO.Playlist;
+import com.dinidininta.spotifyassistant.adapters.PlaylistsAdapter;
+
+import java.util.ArrayList;
+
 public class PlaylistsActivity extends AppCompatActivity {
+    private RecyclerView rvPlaylists;
+    private ArrayList<Playlist> playlists = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlists);
+
+        rvPlaylists = findViewById(R.id.rv_playlists);
+        rvPlaylists.setHasFixedSize(false);
+
+        // populate playlists here?
+        showRecyclerList();
+    }
+
+    private void showRecyclerList(){
+        rvPlaylists.setLayoutManager(new LinearLayoutManager(this));
+        PlaylistsAdapter playlistsAdapter = new PlaylistsAdapter(playlists);
+        rvPlaylists.setAdapter(playlistsAdapter);
     }
 }
---
+++ app/src/main/java/com/dinidininta/spotifyassistant/adapters/PlaylistsAdapter.java
@@ -0,0 +1,53 @@
+package com.dinidininta.spotifyassistant.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dinidininta.spotifyassistant.POJO.Playlist;
+import com.dinidininta.spotifyassistant.POJO.Playlists;
+import com.dinidininta.spotifyassistant.R;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PlaylistsAdapter extends RecyclerView.Adapter<PlaylistsAdapter.ListViewHolder> {
+    private ArrayList<Playlist> playlists;
+
+    public PlaylistsAdapter(ArrayList<Playlist> list){
+        this.playlists = list;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_playlist, parent, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        Playlist playlist = playlists.get(position);
+        holder.playlistTitle.setText(playlist.name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return playlists.size();
+    }
+
+    class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView playlistImage;
+        TextView playlistTitle;
+
+        ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            playlistImage = itemView.findViewById(R.id.playlist_image);
+            playlistTitle = itemView.findViewById(R.id.playlist_title);
+        }
+    }
+}
